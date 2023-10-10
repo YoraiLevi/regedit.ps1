6c5d6473cba9682c2c4d6ae5b152c83ea1b1360a
Yorai Levi <Yorai.email@gmail.com>
2023-10-10 03:24:52 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# reg.ps1
+# regedit.ps1
 
-reg.ps1 is a pure PowerShell implementation for reg.exe, backed by a fully tested and idempotent cmdlets.
+regedit.ps1 is a pure PowerShell implementation for regedit.exe, backed by a fully tested and idempotent cmdlets.
 
 <!-- # REF:
 https://github.com/SegoCode/Reg-importer
 https://github.com/UNT-CAS/ConvertFrom-Registry/blob/master/ConvertFrom-Registry.ps1
 https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/import-csv?view=powershell-7.3
 Get Values (Recursively if desired) from a Registry Key and return them as a Hashtable. https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/import-csv?view=powershell-5.1
+https://support.microsoft.com/en-us/topic/how-to-add-modify-or-delete-registry-subkeys-and-values-by-using-a-reg-file-9c7f37cf-a5e9-e1cd-c4fa-2a26218a1a23#bkmk_syntax
  -->
---
+++ regedit.ps1
@@ -0,0 +1,13 @@
+<# https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/reg
+reg add 	Adds a new subkey or entry to the registry.
+reg compare Compares specified registry subkeys or entries.
+reg copy    Copies a registry entry to a specified location on the local or remote computer.
+reg delete	Deletes a subkey or entries from the registry.
+reg export	Copies the specified subkeys, entries, and values of the local computer into a file for transfer to other servers.
+reg import	Copies the contents of a file that contains exported registry subkeys, entries, and values into the registry of the local computer.
+reg load	Writes saved subkeys and entries into a different subkey in the registry.
+reg query	Returns a list of the next tier of subkeys and entries that are located under a specified subkey in the registry.
+reg restore	Writes saved subkeys and entries back to the registry.
+reg save	Saves a copy of specified subkeys, entries, and values of the registry in a specified file.
+reg unload	Removes a section of the registry that was loaded using the reg load operation.
+#>
